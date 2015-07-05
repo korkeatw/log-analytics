c38c8c5d92de6b2213ac552fc3d9f40831dc3648
khasathan <khasathan@gmail.com>
2015-07-05 13:27:08 +0700

    improve for readable code

--- config/config.js
+++ config/config.js
@@ -17,25 +17,25 @@ config.monitoringUrls = [
     {
         name: 'get:count_pending_messages',
         path: 'GET /api/users/{user_id}/count_pending_messages',
-        urlPattern: 'method=GET path=/api/users/\\d+/count_pending_messages ',
+        urlPattern: ' method=GET path=/api/users/\\d+/count_pending_messages ',
         urlRegExp: new RegExp(/ method=GET path=\/api\/users\/\d+\/count_pending_messages /)
     },
     {
         name: 'get:get_messages',
         path: 'GET /api/users/{user_id}/get_messages',
-        urlPattern: 'method=GET path=/api/users/\\d+/get_messages ',
+        urlPattern: ' method=GET path=/api/users/\\d+/get_messages ',
         urlRegExp: new RegExp(/ method=GET path=\/api\/users\/\d+\/get_messages /)
     },
     {
         name: 'get:get_friends_progress',
         path: 'GET /api/users/{user_id}/get_friends_progress',
-        urlPattern: 'method=GET path=/api/users/\\d+/get_friends_progress ',
+        urlPattern: ' method=GET path=/api/users/\\d+/get_friends_progress ',
         urlRegExp: new RegExp(/ method=GET path=\/api\/users\/\d+\/get_friends_progress /)
     },
     {
         name: 'get:get_friends_score',
         path: 'GET /api/users/{user_id}/get_friends_score',
-        urlPattern: 'method=GET path=/api/users/\\d+/get_friends_score ',
+        urlPattern: ' method=GET path=/api/users/\\d+/get_friends_score ',
         urlRegExp: new RegExp(/ method=GET path=\/api\/users\/\d+\/get_friends_score /)
     },
     {
